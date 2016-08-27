b03b6f80077259775389ada60ec34da376b162c3
Philip Van de Walker <trafflux@gmail.com>
2016-08-27 01:24:13 -0400

    Attempting to play nice with Heroku

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var dataFile = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000);
+app.set('appData', dataFile);
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.locals.siteTitle = 'SiteTitle Awesomeo Website!';
+
+app.use(express.static('./public'));
+app.use(require('./routes/index'));
+app.use(require('./routes/speakers'));
+
+var server = app.listen(app.get('port'), function(){
+	console.log('Node app running on ' + app.get('port'));
+});
